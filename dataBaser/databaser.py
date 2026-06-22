3a282f1e69b91e131e30c2da6120def9629232db
ankurage <ankuridge@gmail.com>
2026-06-22 07:51:04 +0300

    init for database

--- dataBaser/databaser.py
+++ dataBaser/databaser.py
@@ -2,6 +2,9 @@ import os
 import json
 
 class DataBase:
+    def __init__(self, db_name):
+        self.db_name = db_name
+
     def db_format_check(db):
         check = db[::-1]
         if check[:5] == "nosj.":
@@ -10,25 +13,25 @@ class DataBase:
             return False
         
     # == | == #
-    def create(db_name, obj: any, check_exists: bool=True, encode: str="utf-8", ensure_ascii: bool=False, indent: int=4):
+    def create(self, obj: any, check_exists: bool=True, encode: str="utf-8", ensure_ascii: bool=False, indent: int=4):
         if check_exists:
-            if not os.path.exists(db_name):
-                with open(db_name, "w", encoding=encode) as file:
+            if not os.path.exists(self.db_name):
+                with open(self.db_name, "w", encoding=encode) as file:
                     json.dump(obj, file, indent=indent, ensure_ascii=ensure_ascii)
         else:
-            with open(db_name, "w", encoding=encode) as file:
+            with open(self.db_name, "w", encoding=encode) as file:
                 json.dump(obj, file, indent=indent, ensure_ascii=ensure_ascii)
 
     # == | == #
-    def get(db_name, encode: str="utf-8"):
+    def get(self, encode: str="utf-8"):
         try:
-            with open(db_name, "r", encoding=encode) as file:
+            with open(self.db_name, "r", encoding=encode) as file:
                 data = json.load(file)
                 return data
         except json.decoder.JSONDecodeError:
             raise ValueError("Decode Error")
             
     # == | == #
-    def load(db_name, obj: any, encode: str="utf-8", ensure_ascii: bool=False, indent: int=4):
-        with open(db_name, "w", encoding=encode) as file:
+    def load(self, obj: any, encode: str="utf-8", ensure_ascii: bool=False, indent: int=4):
+        with open(self.db_name, "w", encoding=encode) as file:
             json.dump(obj, file, indent=indent, ensure_ascii=ensure_ascii)
